4952d81d23cec4d3672a6d2e8b72a2d650dd2c1f
N.Shimizu <chikoski@gmail.com>
2024-04-15 11:23:13 +0900

    add chikoski:hello-world to the dependency

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 mod bindings;
 
+use bindings::chikoski::hello_world::chikoski_hello_world_greet::hello_world;
+use bindings::chikoski::hello_world::chikoski_hello_world_greet::greet;
+
 fn main() {
     println!("Hello, world!");
-    bindings::hello_world();
+    println!("greet:hello_world() = {}", hello_world());
     
-    let greeting = bindings::greet("Rust");
+    let greeting = greet("Rust");
     println!("{}", greeting);
 }

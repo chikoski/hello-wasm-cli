21fdfc7f1bf61811d86f57daa4a1caf8219f1702
N.Shimizu <chikoski@gmail.com>
2024-05-01 17:49:02 +0900

    Updated to be consistent with the articles on zenn

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,8 @@
 mod bindings;
 
-use bindings::chikoski::hello_world::chikoski_hello_world_greet::hello_world;
-use bindings::chikoski::hello_world::chikoski_hello_world_greet::greet;
+use bindings::chikoski::name::name_provider::name;
 
 fn main() {
-    println!("Hello, world!");
-    println!("greet:hello_world() = {}", hello_world());
-    
-    let greeting = greet("Rust");
-    println!("{}", greeting);
+    let n = name();
+    println!("Hello, {}!", n);
 }
